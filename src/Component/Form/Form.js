3e16422dc0f7ba53d16bc624b5267e54424ea332
victor manuel delfin santos <vdelfin.yunius@gmail.com>
2021-02-20 14:48:37 -0600

    Form

--- src/Component/Form/Form.js
+++ src/Component/Form/Form.js
@@ -1,52 +1,115 @@
-import { useElements, useStripe, CardElement } from '@stripe/react-stripe-js';
-import React from 'react'
-import classes from "./Form.css"
+import React, { useState, useEffect } from "react";
+import {
+  CardElement,
+  useStripe,
+  useElements
+} from "@stripe/react-stripe-js";
+import "./Form.css"
+import { useParams } from "react-router-dom";
 
-const Form = (props)=>{ 
-    const stripe = useStripe();
-    const elements = useElements();
-  
-    const handleSubmit = async event => {
-      event.preventDefault();
-  
-      if (!stripe || !elements) {
-        // Stripe.js has not loaded yet. Make sure to disable
-        // form submission until Stripe.js has loaded.
-        return;
-      }
-  
-      const payload = await stripe.createPaymentMethod({
-        type: "card",
-        card: elements.getElement(CardElement)
+export default function CheckoutForm() {
+  const [succeeded, setSucceeded] = useState(false);
+  const [error, setError] = useState(null);
+  const [processing, setProcessing] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState('');
+  const stripe = useStripe();
+  const elements = useElements();
+  const {name} = useParams("name")
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    window
+      .fetch("http://localhost:3001/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({item: name })
+      })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+          
+        console.log(data);
+        setClientSecret(data.clientSecret);
       });
-  
-      console.log("[PaymentMethod]", payload);
-    };
-  
-    return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          Card details
-          <CardElement
-            
-            // onReady={() => {
-            //   console.log("CardElement [ready]");
-            // }}
-            // onChange={event => {
-            //   console.log("CardElement [change]", event);
-            // }}
-            // onBlur={() => {
-            //   console.log("CardElement [blur]");
-            // }}
-            // onFocus={() => {
-            //   console.log("CardElement [focus]");
-            // }}
-          />
-        </label>
-        <button type="submit" disabled={!stripe}>
-          Pay
-        </button>
-      </form>
-    );
+  }, []);
+
+  const cardStyle = {
+    style: {
+      base: {
+        color: "#32325d",
+        fontFamily: 'Courier, monospace',
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d",
+          backgroundColor:"white"
+        }
+      },
+      invalid: {
+        color: "#fa755a",
+        iconColor: "#fa755a"
+      }
+    }
   };
-export default Form
+  const handleChange = async (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
+
+  const handleSubmit = async ev => {
+    ev.preventDefault();
+    setProcessing(true);
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement)
+      }
+    });
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+    }
+  };
+
+  return (
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <CardElement id="card-element" options={cardStyle} onChange={handleChange} />
+      <button
+        disabled={processing || disabled || succeeded}
+        id="submit"
+      >
+        <span id="button-text">
+          {processing ? (
+            <div className="spinner" id="spinner"></div>
+          ) : (
+            "Pay now"
+          )}
+        </span>
+      </button>
+      {/* Show any error that happens when processing the payment */}
+      {error && (
+        <div className="card-error" role="alert">
+          {error}
+        </div>
+      )}
+      {/* Show a success message upon completion */}
+      <p className={succeeded ? "result-message" : "result-message hidden"}>
+        Payment succeeded, see the result in your
+        <a
+          href={`https://dashboard.stripe.com/test/payments`}
+        >
+          {" "}
+          Stripe dashboard.
+        </a> Refresh the page to pay again.
+      </p>
+    </form>
+  );
+}
